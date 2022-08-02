0f2e74e793fe32815963f4677ffbb533ba207642
Vishaal <80566448+VishaalC@users.noreply.github.com>
2022-08-02 18:09:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,5 +111,7 @@ Thank you.''',
     return render_template('success.html')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
